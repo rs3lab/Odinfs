5e1636d1312da7417d76cc042f806516910f8ea4
Diyu Zhou <zhoudiyupku@gmail.com>
2022-04-16 00:48:23 +0200

    Add gnuplot

--- dep.sh
+++ dep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install libdaxctl-dev libndctl-dev libipmctl-dev libudev-dev libkmod-dev uuid-dev openssh-server vim git make gcc flex bison dpkg-dev libssl-dev htop ndctl python2 libnuma-dev libtool libkmod-dev 
+sudo apt install libdaxctl-dev libndctl-dev libipmctl-dev libudev-dev libkmod-dev uuid-dev openssh-server vim git make gcc flex bison dpkg-dev libssl-dev htop ndctl python2 libnuma-dev libtool libkmod-dev gnuplot 
